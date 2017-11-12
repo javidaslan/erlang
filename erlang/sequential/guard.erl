5a4c5abc0636e1bcc5ba3b84013b19c08ea1bcc2
Javid Aslanov <j.aslanov94@gmail.com>
2017-11-13 00:41:54 +0100

    Git check Fedora

--- erlang/sequential/guard.erl
+++ erlang/sequential/guard.erl
@@ -2,7 +2,7 @@
 -export([factorial/1, guard/2]).
 
 
-factorial(N) 
+factorial(N)
     when N > 0 -> N * factorial(N-1);
 factorial(0) -> 1.
 
@@ -11,5 +11,4 @@ factorial(0) -> 1.
 guard(N, M) when N > 5, M < 5 -> N * M;
 guard(N, M) when N =< 5; M >= 5 -> N + M.
 
-
-
+% Git check %
